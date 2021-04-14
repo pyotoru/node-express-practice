5e98f420657b8741ab709fb02136524997b86e6a
pyotoru <wawrzyniak.peter@gmail.com>
2021-04-15 00:14:47 +0900

    os module

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-// Common JS, every file is module (by default)
-// Modules - Encapsulated code (only share minimum)
+// built-in module
+const os = require("os");
 
-const names = require("./4-names");
-const sayHi = require("./5-utils");
-const data = require("./6-alternative-flavor");
-require("./7-mind-grenade");
-// sayHi("susan");
-// sayHi(names.john);
-// sayHi(names.peter);
+// info about current user
+const user = os.userInfo();
+console.log(user);
+// method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()}seconds`);
+
+const currentOS = {
+	name: os.type(),
+	release: os.release(),
+	totalMem: os.totalmem(),
+	freeMem: os.freemem(),
+};
+console.log(currentOS);
