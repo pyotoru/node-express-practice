e99a19b535dcebc79670508ff33c8cde23137496
pyotoru <wawrzyniak.peter@gmail.com>
2021-04-17 22:37:36 +0900

    npm

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const http = require("http");
+// npm -global command, comes with node
+// npm --version
 
-const server = http.createServer((req, res) => {
-	if (req.url === "/") {
-		res.end("Welcome to our home page");
-	}
-	if (req.url === "/about") {
-		res.end("Here is our short history");
-	}
-	res.end(`<h1>Oops!</h1>
-<p>We can't seem to find the page you are looking for</p>
-<a href="/">back home</a>`);
-});
+// local dependency - use it only in this particular project
+// npm i <packageName>
 
-server.listen(5000);
+// global dependency - use it in any project
+// npm install -g <packageName>
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+const _ = require("lodash");
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
