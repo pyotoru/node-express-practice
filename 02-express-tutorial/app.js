5bdf6cd503bf23b979f5bdd190330e2eb45fab7b
pyotoru <wawrzyniak.peter@gmail.com>
2021-05-13 22:16:29 +0900

    api products

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,7 +3,15 @@ const app = express();
 
 const { products } = require("./data.js");
 app.get("/", (req, res) => {
-	res.json(products);
+	res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+	const newProducts = products.map((products) => {
+		const { id, name, image } = products;
+		return { id, name, image };
+	});
+	res.json(newProducts);
 });
 
 app.listen(5000, () => {
