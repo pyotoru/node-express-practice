75cfb9a60fb3ac07f37bb5b8f3722c6c59749045
pyotoru <wawrzyniak.peter@gmail.com>
2021-05-15 16:40:22 +0900

    query string

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -18,6 +18,7 @@ app.get("/api/products/:productID", (req, res) => {
 	// console.log(req);
 	// console.log(req.params);
 	const { productID } = req.params;
+
 	const singleProduct = products.find(
 		(product) => product.id === Number(productID)
 	);
@@ -25,10 +26,29 @@ app.get("/api/products/:productID", (req, res) => {
 		return res.status(404).send("Product Does Not Exist");
 	}
 
-	return;
+	return res.json(singleProduct);
+});
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+	console.log(req.params);
+});
 
-	console.log(singleProduct);
-	res.json(singleProduct);
+app.get("/api/v1/query", (req, res) => {
+	const { search, limit } = req.query;
+	let sortedProducts = [...products];
+
+	if (search) {
+		sortedProducts = sortedProducts.filter((product) => {
+			return product.name.startsWith(search);
+		});
+	}
+	if (limit) {
+		sortedProducts = sortedProducts.slice(0, Number(limit));
+	}
+	if (sortedProducts.length < 1) {
+		// res.status(200).send("no products matched your search");
+		return res.status(200).json({ success: true, data: [] });
+	}
+	res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
